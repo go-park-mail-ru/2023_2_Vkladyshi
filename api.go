d85eed8affed6796dcc7b2f6b5ab2e71dc0de4bb
kingjoram <alex.v.danilchenko@yandex.ru>
2023-10-02 08:44:29 +0300

    21151804: add more films

--- api.go
+++ api.go
@@ -29,10 +29,14 @@ type FilmsResponse struct {
 func addFilms() []Film {
 	films := []Film{}
 
+	films = append(films, Film{"Леди Баг и Супер-Кот: Пробуждение силы", "/static/img/ladybag.png", 7.5, []string{"комедия", "приключения", "фэнтези", "мелодрама"}})
 	films = append(films, Film{"Барби", "/static/img/barbie.png", 6.7, []string{"комедия", "приключения", "фэнтези"}})
 	films = append(films, Film{"Опенгеймер", "/static/img/oppenheimer.png", 8.5, []string{"биография", "драма", "история"}})
 	films = append(films, Film{"Слуга народа", "/static/img/sluga_naroda.png", 0.7, []string{"комедия"}})
 	films = append(films, Film{"Черная Роза", "/static/img/black_rose.png", 1.5, []string{"детектив", "триллер", "криминал"}})
+	films = append(films, Film{"Бесславные ублюдки", "/static/img/inglourious_basterds.png", 8.0, []string{"боевик", "военный", "драма", "комедия"}})
+	films = append(films, Film{"Бэтмен: Начало", "/static/img/batman_begins.png", 7.9, []string{"боевик", "фантастика", "драма", "приключения"}})
+	films = append(films, Film{"Криминальное чтиво", "/static/img/pulp_fiction.png", 8.6, []string{"криминал", "драма"}})
 	films = append(films, Film{"Терминатор", "/static/img/terminator.png", 8.0, []string{"боевик", "фантастика", "триллер"}})
 
 	return films
@@ -50,7 +54,7 @@ func (a *API) Films(w http.ResponseWriter, r *http.Request) {
 	filmsResponse := FilmsResponse{
 		Page:     page,
 		Total:    uint64(len(films)),
-		Films:    films,
+		Films:    films[pageSize*(page-1) : pageSize*page],
 		PageSize: int(pageSize),
 	}
 	response := Response{
