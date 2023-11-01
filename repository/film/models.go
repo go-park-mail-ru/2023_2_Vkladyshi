1f1b09eb3fb3863146e67ff4bae1a499a0649d9f
kingjoram <alex.v.danilchenko@yandex.ru>
2023-11-01 15:54:52 +0300

    23680961: models to their repositories

---
+++ repository/film/models.go
@@ -0,0 +1,11 @@
+package film_repo
+
+type FilmItem struct {
+	Id          int
+	Title       string
+	Info        string
+	Poster      string
+	ReleaseDate string
+	Country     string
+	Mpaa        string
+}
---
+++ repository/user/models.go
@@ -0,0 +1,11 @@
+package user_repo
+
+type UserItem struct {
+	Id               int
+	name             string
+	Birthdate        string
+	Photo            string
+	Login            string
+	Password         string
+	RegistrationDate string
+}
