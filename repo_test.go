8797339bfa34c674bd7ce2230a7a9a315f5850f1
kingjoram <alex.v.danilchenko@yandex.ru>
2023-11-06 19:18:10 +0300

    23680961: get film rating test

--- repo_test.go
+++ repo_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-park-mail-ru/2023_2_Vkladyshi/repository/comment"
 	"github.com/go-park-mail-ru/2023_2_Vkladyshi/repository/film"
 	"github.com/go-park-mail-ru/2023_2_Vkladyshi/repository/profile"
 )
@@ -338,6 +339,56 @@ func TestGetFilm(t *testing.T) {
 		WillReturnError(fmt.Errorf("db_error"))
 
 	_, err = repo.GetFilm(1)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+	if err == nil {
+		t.Errorf("expected error, got nil")
+		return
+	}
+}
+
+func TestGetFilmRating(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"Average"})
+
+	expect := 4.2
+
+	rows = rows.AddRow(expect)
+
+	mock.ExpectQuery("SELECT").WithArgs(1).WillReturnRows(rows)
+
+	repo := &comment.RepoPostgre{
+		DB: db,
+	}
+
+	rating, err := repo.GetFilmRating(1)
+	if err != nil {
+		t.Errorf("GetFilm error: %s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+
+	if rating != expect {
+		t.Errorf("results not match, want %v, have %v", expect, rating)
+		return
+	}
+
+	mock.
+		ExpectQuery("SELECT").
+		WithArgs(1).
+		WillReturnError(fmt.Errorf("db_error"))
+
+	_, err = repo.GetFilmRating(1)
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 		return
